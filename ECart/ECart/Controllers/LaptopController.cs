972c32090972a9627abb59c29071cc81460793de
Nareshkumar1996 <56652240+Nareshkumar1996@users.noreply.github.com>
2020-07-23 01:16:46 +0530

    Initial commit - Laptop module
    1.Created tables in DB. (Laptop and Laptop Category)
    2.Installed EntityFramework.
    2.Using DB first approach created ADO.NET Entity Data Model.
    3.Created Navbar to access the laptop category.
    4. Added CustomStyle.css using Bundle

---
+++ ECart/ECart/Controllers/LaptopController.cs
@@ -0,0 +1,36 @@
+﻿using ECart.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECart.Controllers
+{
+    public class LaptopController : Controller
+    {
+        ECartEntities1 eCart = new ECartEntities1();
+        public ActionResult Index()
+        {
+            var categories = eCart.LaptopCategories;
+            return View(categories);
+        }
+        public ActionResult NavBar()
+        {
+            
+            List<LaptopCategory> categories = eCart.LaptopCategories.ToList();
+            LaptopViewModel laptopViewModel = new LaptopViewModel();
+            laptopViewModel.LaptopCategories = categories;
+            return PartialView(laptopViewModel);
+        }
+        
+        public ActionResult List(int categoryid)
+        {
+           
+            List<Laptop> laptops = eCart.Laptops.Where(c => c.LapCategoryId == categoryid).ToList();
+            LaptopViewModel laptopView = new LaptopViewModel();
+            laptopView.Laptops = laptops;
+            return View(laptopView);
+        }
+    }
+}
---
+++ ECart/ECart/ViewModels/LaptopViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ECart.ViewModels
+{
+    public class LaptopViewModel
+    {
+        public List<Laptop> Laptops { get; set; }
+        public List<LaptopCategory> LaptopCategories { get; set; }
+
+    }
+}
